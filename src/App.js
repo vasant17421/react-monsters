a742bd769a3b4f09851192355e92ed465d4a329a
Vasant Raina <rvasant@teqmavens.com>
2021-06-07 21:58:39 +0530

    adding files in react monster project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+
+      string: 'Hello Pops',
+      
+      monsters:[],
+      searchField:''
+    };
+    //this.handleChange=this.handleChange.bind(this);//this is replaced by arrow function
+  }
+  componentDidMount(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(alpa=>this.setState({monsters:alpa}))
+    .catch(reject=>console.log('there is error'));
+  }
+
+  handleChange= syn => {
+
+    this.setState({searchField:syn.target.value} );
+
+  }
+  render() {
+    const { monsters,searchField } =this.state;
+   
+    //const  monsters  =this.state.monsters;
+    //const  searchField  =this.state.searchField;
+     const filteredMonsters=monsters.filter(monster =>monster.name.toLowerCase().includes(searchField.toLowerCase()));
+     return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Roller Monsters</h1>
+      <SearchBox placeholder='search monsters'
+      handleChange={this.handleChange}
+      >
+     
+      </SearchBox>
+      {/* <input type='search' placeholder='search here' onChange={ syn => { this.setState({searchField:syn.target.value} ) }}/> */}
+            <CardList monsters={filteredMonsters}>
+     
+      </CardList>
+      
     </div>
   );
+  }
 }
 
 export default App;
